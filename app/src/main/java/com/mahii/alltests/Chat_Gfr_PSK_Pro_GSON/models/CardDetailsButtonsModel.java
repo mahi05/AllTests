04484335b9fb64751d7b8f4462b67ccb449bf939
Mahesh Babariya <mahi.mb1991@gmail.com>
2016-09-28 12:40:19 +0530

    Gson related test

---
+++ app/src/main/java/com/mahii/alltests/Chat_Gfr_PSK_Pro_GSON/models/CardDetailsButtonsModel.java
@@ -0,0 +1,32 @@
+package com.mahii.alltests.Chat_Gfr_PSK_Pro_GSON.models;
+
+public class CardDetailsButtonsModel {
+
+    private String title;
+    private String type;
+    private String value;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
