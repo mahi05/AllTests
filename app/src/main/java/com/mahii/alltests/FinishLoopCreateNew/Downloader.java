410b255218c881641557fac634991200c6a51d9a
Mahesh Babariya <mahi.mb1991@gmail.com>
2016-10-01 14:28:40 +0530

    Testing ION Downloader

--- app/src/main/java/com/mahii/alltests/FinishLoopCreateNew/Downloader.java
+++ app/src/main/java/com/mahii/alltests/FinishLoopCreateNew/Downloader.java
@@ -1,10 +1,11 @@
 package com.mahii.alltests.FinishLoopCreateNew;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
@@ -12,35 +13,66 @@ import com.koushikdutta.ion.ProgressCallback;
 
 import java.io.File;
 
-public class Downloader extends AppCompatActivity {
+public class IonDownloader extends AppCompatActivity {
 
-    ProgressDialog progressDialog;
+    String[] links = {
+            "https://s3.amazonaws.com/tabrideadvertisements/2016_09_30_22_09_52.mp4",
+            "https://s3.amazonaws.com/tabrideadvertisements/2016_09_30_22_09_55.mp4",
+            "https://s3.amazonaws.com/tabrideadvertisements/2016_09_30_22_09_20.mp4",
+            "https://s3.amazonaws.com/tabrideadvertisements/2016_09_30_01_09_13.mp4"
+    };
+
+    String[] names = {
+            "2016_10_01_12_39_00_1.mp4",
+            "2016_10_01_12_39_00_2.mp4",
+            "2016_10_01_12_39_00_3.mp4",
+            "2016_10_01_12_39_00_4.mp4"
+    };
+
+    int m = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Downloading");
-        progressDialog.show();
+        for (int i = 0; i < links.length; i++) {
 
-        Ion.with(this)
-                .load("https://s3.amazonaws.com/tabrideadvertisements/2016_09_14_01_09_51_SampleVideo.mp4")
-                .progressDialog(progressDialog)
+            Ion.with(IonDownloader.this)
+                    .load(links[i])
+                    .progress(new ProgressCallback() {
+                        @Override
+                        public void onProgress(long downloaded, long total) {
+                            Log.e("Downloading", "" + downloaded + " / " + total);
+                        }
+                    })
+                    .write(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), names[i]))
+                    .setCallback(new FutureCallback<File>() {
+                        @Override
+                        public void onCompleted(Exception e, File file) {
+                            //mCallback.onDownloadComplete(file);
+                            Toast.makeText(IonDownloader.this, "Download No " + m, Toast.LENGTH_SHORT).show();
+                            Log.e("Download No", "" + m);
+                            m++;
+                        }
+                    });
+
+        }
+
+        /*Ion.with(IonDownloader.this)
+                .load("https://s3.amazonaws.com/tabrideadvertisements/2016_09_14_23_09_43.mp4")
                 .progress(new ProgressCallback() {
                     @Override
                     public void onProgress(long downloaded, long total) {
-                        //System.out.println("" + downloaded + " / " + total);
-                        Log.e("Total", "" + downloaded + " / " + total);
+                        Log.e("Downloading", "" + downloaded + " / " + total);
                     }
                 })
-                .write(new File("/sdcard/xyz.mp4"))
+                .write(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "2016_09_14_23_09_43.mp4"))
                 .setCallback(new FutureCallback<File>() {
                     @Override
                     public void onCompleted(Exception e, File file) {
-                        progressDialog.dismiss();
+                        Toast.makeText(IonDownloader.this, "Download Complete", Toast.LENGTH_SHORT).show();
                     }
-                });
+                });*/
 
     }
 }
